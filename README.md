b696ca0f4feb30fde1b89c27638feda190832567
piotrpdev <peterplaczek03@gmail.com>
2023-06-06 23:09:49 +0100

    final touches

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ docker build -t suntracker:v1 .
 
 - [k8s using kind](https://www.baeldung.com/ops/kubernetes-kind)
 - [docker and nginx](https://www.dailysmarty.com/posts/steps-for-deploying-a-static-html-site-with-docker-and-nginx)
+- [static site on k8s](https://technicallysound.in/how-to-setup-a-static-site-on-kubernetes/)
 - [nginx mime type issue fix](https://github.com/storybookjs/storybook/issues/20157#issuecomment-1464901423)
--- kind_start.sh
+++ kind_start.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 sudo kind create cluster --config suntrackerConfig.yaml && \
 sudo kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml && \
-sudo kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission && \
-sleep 10 && \
+sudo kubectl wait --namespace ingress-nginx --for=condition=ready pod --selector=app.kubernetes.io/component=controller --timeout=90s && \
+# sudo kubectl delete -A ValidatingWebhookConfiguration ingress-nginx-admission && \
+# sleep 10 && \
 sudo kubectl apply -f suntrackerService.yaml
-# sudo kind delete cluster --name suntracker-kind
+# sudo kind delete cluster
